444d3e2d6e71950876d21db7a7e104189bb48589
Avneet Singh Sohi <79573795+avneetsohi@users.noreply.github.com>
2024-03-23 19:53:39 +1100

    Second Commit - Updated server/index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://study-notion-edtech-amber.vercel.app",
         credentials:true
     })
 )
